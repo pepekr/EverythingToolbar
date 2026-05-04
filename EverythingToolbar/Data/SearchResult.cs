84fb59d5e455aba8e5dcbdf94881b2f893e50acb
porokhniavitalii <legenda.repa4@gmail.com>
2026-05-04 21:32:14 +0300

    chore: remove unused usings

--- EverythingToolbar/Data/SearchResult.cs
+++ EverythingToolbar/Data/SearchResult.cs
@@ -5,7 +5,6 @@ using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
 using System.IO.Pipes;
-using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Security.Principal;
--- EverythingToolbar/Helpers/StartMenuIntegration.cs
+++ EverythingToolbar/Helpers/StartMenuIntegration.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Runtime.InteropServices;
 using System.Text;
-using System.Windows.Input;
 using System.Windows.Threading;
 using NLog;
 
